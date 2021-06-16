7f96755e9a54359eb62846339e39b57a27975418
Margarita Morozova <ritamorozova@yahoo.com>
2021-06-16 08:05:14 -0700

    add linear search

---
+++ linear_search.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int numbers[] = {4, 6, 8, 5, 1, 0};
+
+    for (int i = 0; i < 7; i++)
+    {
+        if(numbers[i] == 0)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
+
+
